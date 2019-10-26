d07538aa4d540964426af21f13ea502e6689e549
Justin Jetwiriyanon <jittarinjetwiriyanon@justins-MacBook-Air.local>
2019-10-26 14:00:50 +1300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { CoinComponent } from './pages/coin/coin.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'coin', component: CoinComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
